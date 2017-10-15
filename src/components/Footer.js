1ef3d41adf6b956fcfdac189cec20c44031aaaa4
Keith Wong <keith.wong3672@gmail.com>
2017-10-15 10:06:02 -0700

    Created Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Grid, Row} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+// import { Icon } from 'react-font-awesome';
+import './Footer.css';
 
 class Footer extends React.Component {
   constructor() {
@@ -7,7 +12,18 @@ class Footer extends React.Component {
   }
 
   render() {
-    return <p>Footer</p>;
+    return (
+
+      <Row md={12} className="footer" style={styles.footer}>
+        <Col md={4} className="text-center" style={styles.copyright}>Copyright © All Rights Reserved</Col>
+        <Col md={4} className="text-center" style={styles.contact}>Telephone: (925) 230-9484</Col>
+        <Col md={4} className="text-center">
+          <div style={styles.socialMedia}><a href="https://www.facebook.com"><i className="fa fa-facebook"/></a></div>
+          <div style={styles.socialMedia}><a href="https://plus.google.com"><i className="fa fa-google-plus"/></a></div>
+          <div style={styles.socialMedia}><a href="https://www.twitter.com"><i className="fa fa-twitter"/></a></div>
+        </Col>
+      </Row>
+    )
   }
 
   componentDidMount() {
@@ -16,3 +32,33 @@ class Footer extends React.Component {
 }
 
 export default Footer;
+
+
+const styles={
+  footer: {
+    backgroundColor: 'rgba(102, 102, 102, 1)',
+    border: '2px inset rgba(64, 58, 53, 1)',
+    color: 'rgba(255, 255, 255, 1)',
+  },
+  copyright: {
+    fontSize: '14px',
+    padding: '10px 0 0 0',
+  },
+  contact: {
+    fontSize: '28px',
+  },
+  socialMedia: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(102, 102, 102, 1)',
+    border: '1px solid rgba(255, 255, 255, 1)',
+    borderRadius: '25px',
+    display: 'inline-flex',
+    fontSize: '14px',
+    height: '25px',
+    justifyContent: 'center',
+    margin: '8px',
+    width: '25px',
+
+  },
+}
+
